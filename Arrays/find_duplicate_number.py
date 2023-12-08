011af7cbcfd9dbf16d826491882cbe78dcc9b804
Dipti <diptisinghbhadouriya@gmail.com>
2023-12-08 22:02:45 +0530

    check if  array has duplicate

--- Arrays/find_duplicate_number.py
+++ Arrays/find_duplicate_number.py
@@ -11,9 +11,16 @@ def duplicate(number):
 
     print(duplicate_list)
         
-
-
-
-
 number = [1, 2, 3, 3, 5, 7]
 duplicate(number)
+
+# Sort the Array and iterate to n-1 and check if nums[i]==nums[i+1] and return nums[i]
+def findDuplicate(nums: list[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        for i in range(n-1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+                
+nums = [1,3,4,2,2]
+findDuplicate(nums)
