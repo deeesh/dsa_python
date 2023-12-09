e0b69e512460a06d761d59149e31bd23614ecc72
Dipti <diptisinghbhadouriya@gmail.com>
2023-12-09 14:10:09 +0530

    Merge Intervals

---
+++ Arrays/merge_intervals.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def merge(self, intervals:list[list[int]]) -> list[list[int]]:
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x:x[0])
+        merged = [intervals[0]]
+
+        for i in range(1 , len(intervals)):
+            current_start, current_end = intervals[i]
+            last_start, last_end = merged[-1]
+
+            if current_start<last_end:
+                merged[-1] = [last_start, max(last_end, current_end)]
+            else:
+                merged.append([current_start, current_end])
+        return merged
+
