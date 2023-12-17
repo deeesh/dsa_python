5d4a26fb614edca443b8dc841e65c2f43d24ef62
Dipti <diptisinghbhadouriya@gmail.com>
2023-12-17 22:54:24 +0530

    Max Sum Without Adjacents

---
+++ Arrays/max_sum_without-ajdacents.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+class Solution:
+	
+	def findMaxSum(self,arr, n):
+		# code here
+		if n == 0:
+            return 0
+        if n == 1:
+            return arr[0]
+    
+        incl = arr[0]
+        excl = 0
+    
+        for i in range(1, n):
+            new_excl = max(incl, excl)
+            incl = excl + arr[i]
+            excl = new_excl
+    
+        return max(incl, excl)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.findMaxSum(arr, n)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
